0d8ee88803482ef1f74be5e02b16356cfdb481ae
justynak23 <justynak@spacelift.io>
2025-08-27 09:43:26 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "spacelift" {}
+
+variable "idp_group_mapping_id" {}
+variable "role_id" {}
+variable "target_space_id" {
+  default = "QA-SPACE-ID" # replace with real QA space ID
+}
+
+resource "spacelift_role_attachment" "repro" {
+  idp_group_mapping_id = var.idp_group_mapping_id
+  role_id              = var.role_id
+  space_id             = var.target_space_id
+}
