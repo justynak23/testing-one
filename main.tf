a445aa5dde054b500f6e7ae4d3384dfdbf72df1a
justynak23 <justynak@spacelift.io>
2025-08-27 11:35:08 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,5 @@
-provider "spacelift" {}
-
-variable "idp_group_mapping_id" {}
-variable "role_id" {}
-variable "target_space_id" {
-  default = "QA-SPACE-ID" 
-}
-
-resource "spacelift_role_attachment" "repro" {
-  idp_group_mapping_id = var.idp_group_mapping_id
-  role_id              = var.role_id
-  space_id             = var.target_space_id
-}
+resource "spacelift_stack" "app" {
+# Required:
+name = "app-prod"
+repository = "app-infra"
+branch = "main"
