7ff918e4fe547c3af3c62288ea95f9bb6d0a8e25
justynak23 <justynak@spacelift.io>
2025-08-27 11:51:42 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,3 +8,10 @@ resource "spacelift_context" "prod-k8s-ie" {
   description = "Configuration details for the compute cluster in 🇮🇪"
   name = "Production cluster (Ireland)"
 }
+
+resource "spacelift_context_attachment" "attachment" {
+  context_id = "prod-k8s-ie"
+  stack_id = "spacelift_stack.app.id"
+  Getting Started with TF + Spacelift 3
+  priority = 0
+}
